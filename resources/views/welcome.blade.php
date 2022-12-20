0a0591bb57ab2d92c31278216de8145d45a7a1d1
mdgonzales-upou <113954497+mdgonzales-upou@users.noreply.github.com>
2022-12-20 20:34:10 +0800

    Check SSL

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,7 +71,7 @@
                 <div class="flex justify-center mt-4 sm:items-center sm:justify-between">
                     <div class="text-center text-sm text-gray-500 sm:text-left">
                         <div class="flex items-center">
-                            &copy; {{ date('Y') }} Mon Gonzales
+                            &copy; {{ date('Y') }} Mon Gonzales - E
                         </div>
                     </div>
 
